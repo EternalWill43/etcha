1e79c630fd4c1c2fad605e2ab21eb44af92b3d52
EternalWill43 <emilsmoney@live.com>
2021-11-15 04:57:27 -0500

    2.0

--- myscript.js
+++ myscript.js
@@ -1,27 +1,44 @@
+let container = document.querySelector('.container');
+
 function paint(div) {
-    div.setAttribute('style', 'background-color: black;');
+    div.style.backgroundColor = "black";
+}
+
+function createGrid(gridAmount) {
+    for (let i = 0; i < gridAmount; i++) {
+        const row = document.createElement('div');
+        container.appendChild(row);
+
+        for (let j = 0; j < gridAmount; j++) {
+            const square = document.createElement('div');
+            square.setAttribute('style', 'background-color: white;');
+            square.classList.add('square');
+            square.style.width = `${960 / gridAmount}px`;
+            square.style.height = `${960 / gridAmount}px`;
+            row.appendChild(square);
+        }
+    }
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", () => { paint(square) }, true);
+    });
 }
 
 
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      container.appendChild(cell).className = "grid-item";
-    };
-    let squares = document.querySelectorAll('.grid-item');
-    squares.forEach(square => {
-        square.addEventListener("mouseenter", () => {paint(square)}, {propagate: false});
+function getSize() {
+    num = parseInt(document.querySelector(".inp").value);
+    allboxes = document.querySelectorAll('.square');
+    allboxes.forEach(box => {
+        box.remove();
     })
-  };
-  
-  makeRows(32,32);
+    console.log(num);
+    createGrid(num);
+}
 
-  function unpaint() {
-      let squares = document.querySelectorAll('.grid-item');
-      squares.forEach(square => {
-        square.setAttribute('style', 'background-color: white;');
-      });
-  }
-
+function cleargrid() {
+    squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    })
+}
+getSize();
